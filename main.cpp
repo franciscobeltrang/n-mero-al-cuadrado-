b0fc6485062178b6206935b841bfcd6d857bc758
franciscobeltrang <113156431+franciscobeltrang@users.noreply.github.com>
2022-09-11 22:53:58 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero_entero;
+    std::cout << "ingresa un numero entero" << std::endl;
+    std::cin >> numero_entero;
+    std::cout << "el doble de ese numereo es:"<< numero_entero+numero_entero << std::endl;
+    return 0;
+}
+//estilos para nombrar variables
+// por favor disculpe mi querida tia saly
